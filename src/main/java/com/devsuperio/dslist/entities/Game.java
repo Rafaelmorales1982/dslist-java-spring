bf74b137a29c9bf4fd054531359f6d606b3994d8
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2024-11-12 00:59:28 -0300

    Colocando informações no banco de dados insert Game seed

--- src/main/java/com/devsuperio/dslist/entities/Game.java
+++ src/main/java/com/devsuperio/dslist/entities/Game.java
@@ -30,9 +30,13 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")//tipo texto
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")//tipo texto
 	private String longDescription;
 	
 	//método construtor sem argumentos
@@ -41,14 +45,15 @@ public class Game {
 	}
 
 	//construtor com argumentos
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		//this.id e do objeto e id sozinho é o argumento que esta no parâmetro
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -89,11 +94,21 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
 	public void setPlatform(String platform) {
-		this.platform = platform;
+		this.platforms = platforms;
+	}
+	
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
